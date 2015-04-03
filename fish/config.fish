c16307f8456207dcc496880d146b0037907e9e1e
naoyoshi.okamae <naoyoshi.okamae@livesense.co.jp>
2015-04-03 14:23:58 +0900

    rbenv

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,9 @@
 set fish_dir $HOME/.config/fish
 set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
-set -x LESS='-R'
+set -x LESS '-R'
 set prompt_user "(๑❛ᴗ❛๑)"
+
+# rbenv
+set -x PATH $HOME/.rbenv/bin $PATH
+set -x PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
